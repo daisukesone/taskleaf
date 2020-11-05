089c4d01a55ad71dda8f1b3710fc47760c59260f
daisukesone <sonedai0213@gmail.com>
2020-11-05 22:18:14 +0900

    move ja.yml

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,9 +6,21 @@ class TasksController < ApplicationController
   def show
   end
 
+  def create 
+    tesk = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
   def new
   end
 
   def edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
